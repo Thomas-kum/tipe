18b9a3c19ca09e8905f06634108828c5ed5b3cc8
Thomas <126348654+Thomas-kum@users.noreply.github.com>
2023-06-01 15:14:17 +0200

    Modifications - Pygame
    Ajouts et améliorations du programme de jeu pygame :
     - Corrections des erreurs de bordures ;
     - Corrections des erreurs de sortie de cadre ;
     - Ajout de la restriction de marche arrière ;
     - Correction des positions et des repères.
    
    Signed-off-by: Thomas <126348654+Thomas-kum@users.noreply.github.com>

--- jeu_voiture.py
+++ jeu_voiture.py
@@ -5,13 +5,22 @@ import math
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
-car_x = 550
-car_y = 300
+car_x_debut=740
+car_y_debut=320
+car_acceleration=0.05
+car_freinage=0.1
+
+nb_tours=0
+
+car_x = car_x_debut
+car_y = car_y_debut
 car_speed = 0
-car_angle = -90
+car_angle = 0
+car_rotation = 3
 
 car_image = pygame.image.load("voiture.png")
-car_image = pygame.transform.scale(car_image, (50, 50))
+car_image = pygame.transform.scale(car_image, (30, 50))
+car_image = pygame.transform.rotate(car_image, car_angle)
 
 circuit_image = pygame.image.load("circuit.png")
 circuit_image = pygame.transform.scale(circuit_image, (800, 600))
@@ -23,33 +32,58 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
-    if keys[K_LEFT]:
-        car_angle -= 5
-    if keys[K_RIGHT]:
-        car_angle += 5
+    if keys[K_LEFT] and car_speed!=0:
+        car_angle -= car_rotation
+    if keys[K_RIGHT] and car_speed!=0:
+        car_angle += car_rotation
     if keys[K_UP]:
-        car_speed += 0.1
-    if keys[K_DOWN]:
-        car_speed -= 0.1
+        car_speed += car_acceleration
+    if keys[K_DOWN] and car_speed>0:
+        car_speed -= car_freinage
 
-    car_x += car_speed * math.cos(math.radians(car_angle))
-    car_y += car_speed * math.sin(math.radians(car_angle))
+    car_angle=car_angle%360
+    
+    car_x += car_speed * math.sin(math.radians(car_angle))
+    car_y -= car_speed * math.cos(math.radians(car_angle))
+
+    if car_speed <0:
+        car_speed=0
+
+    if not (5<=car_x<=795 and 5<=car_y<=595):
+        print("")
+        ValueError: print("Sortie de cadre !")
+        print(car_rect)
+        car_x = car_x_debut
+        car_y = car_y_debut
+        car_speed = 0
+        car_angle = 0
 
     car_rect = car_image.get_rect(center=(car_x, car_y))
-    if circuit_image.get_at((car_rect.x, car_rect.y)) == (0, 0, 0):
-        print("Echec -> Remise à zéro !")
+    #if circuit_image.get_at((car_rect.x, car_rect.y)) == (0, 0, 0):
+    if circuit_image.get_at((int(car_x), int(car_y))) == (0, 0, 0):
+
+        print("")
+        print("Echec -> Circuit !")
         print(car_rect)
-        car_x = 550
-        car_y = 300
+        print(car_x,car_y)
+        car_x = car_x_debut
+        car_y = car_y_debut
         car_speed = 0
-        car_angle = -90
+        car_angle = 0
+        print("Tours : ",nb_tours)
+        nb_tours=0
+    
+    if circuit_image.get_at((int(car_x), int(car_y))) == (76, 255, 0):
+        nb_tours+=1
+
 
     screen.blit(circuit_image, (0, 0))
-    rotated_car_image = pygame.transform.rotate(car_image, car_angle)
+    rotated_car_image = pygame.transform.rotate(car_image, -car_angle)
     car_rect = rotated_car_image.get_rect(center=(car_x, car_y))
     screen.blit(rotated_car_image, car_rect)
 
     pygame.display.update()
-    pygame.time.Clock().tick(60)
+    pygame.time.Clock().tick(90)
 
+print(nb_tours)
 pygame.quit()
