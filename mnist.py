ae703e678ddcecf1ebdbdb00a4d084c6ef5e0b22
Thomas <126348654+Thomas-kum@users.noreply.github.com>
2024-05-12 21:31:29 +0200

    Reseau V1
    Signed-off-by: Thomas <126348654+Thomas-kum@users.noreply.github.com>

--- mnist.py
+++ mnist.py
@@ -1,21 +1,22 @@
+from reseau import ReseauConvolutif
+import numpy as np
 import keras
 from keras import layers
 
-import reseau
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
 
-x_train = x_train.reshape((60000, 28 * 28))
-x_test = x_test.reshape((10000, 28 * 28))
-
-print('x_train.shape',x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+assert x_train.shape == (60000, 28, 28)
+print("Données d'entrainement : nombre de données ", x_train.shape[0], " ; taille de l'image ", x_train[0].shape[0], x_train[0].shape[1])
+print("Données de test : nombre de données ", x_test.shape[0], " ; taille de l'image ", x_test[0].shape[1], x_test[0].shape[1])
 
 y_train = keras.utils.to_categorical(y_train)
 y_test = keras.utils.to_categorical(y_test)
 
-reseau([10, 10, 10], x_train, y_train, x_test, y_test, 0.1, 10, 1000)
+
+reseau_mnist = ReseauConvolutif([28*28, 28*28, 10],np.random.rand(),"sigmoid",(x_train, y_train), (x_test, y_test))
+reseau_mnist.entrainement()
+reseau_mnist.test()
--- reseau.py
+++ reseau.py
@@ -8,128 +8,131 @@ def tanh(x):
     return np.tanh(x)
 def derivee_sigmoid(x):
     return sigmoid(x) * (1 - sigmoid(x))
+def derivee_reLu(x):
+    return 1 if x > 0 else 0
+def derivee_tanh(x):
+    return 1 - tanh(x) ** 2
 def derivee_cout(valeurs_theoriques, valeurs_pratiques):
     return 2 * (valeurs_pratiques - valeurs_theoriques)
-def sigmoid_V(M):
-    return np.vectorize(sigmoid)(M)
-def reLu_V(M):
-    return np.vectorize(reLu)(M)
-def tanh_V(M):
-    return np.vectorize(tanh)(M)
+def ligne_to_colonne(M):
+    return M.reshape(M.shape[0] , 1)
+def matrice_to_colonne(M):
+    return M.reshape(M.shape[0] * M.shape[1], 1)
+
+class ReseauConvolutif:
+
+    def __init__(self, info, taux, fonction_nom, data_train, data_test):
+        self.informations_reseau = info
+        self.taux_apprentissage = taux
+
+        if fonction_nom == "sigmoid":
+            self.fonction = np.vectorize(sigmoid)
+            self.fonction_derivee = np.vectorize(derivee_sigmoid)
+        elif fonction_nom == "reLu":
+            self.fonction = np.vectorize(reLu)
+            self.fonction_derivee = np.vectorize(derivee_reLu)
+        else:
+            self.fonction = np.vectorize(tanh)
+            self.fonction_derivee = np.vectorize(derivee_tanh)
+
+        self.x_train, self.y_train = data_train
+        self.x_test, self.y_test = data_test
+
+        self.A, self.V, self.W, self.B = self.definition_reseau()
+
+        self.taille_jeu_entrainement = len(self.x_train)
+
+        self.nombre_succes = 0
+        self.taille_jeu_test = len(self.x_test)
 
 
-def definition_reseau(informations_reseau):
-    V, A, W, B = [], [], [], []
-    A.append(np.array([[None] for _ in range(informations_reseau[0])]))
-    V.append(np.array([[None] for _ in range(informations_reseau[0])]))
-    for i in range(len(informations_reseau) - 1):
-        A.append(np.array([[None] for _ in range(informations_reseau[i + 1])]))
-        V.append(np.array([[None] for _ in range(informations_reseau[i + 1])]))
-        W.append(np.array([[np.random.random_sample() for _ in range(informations_reseau[i])] for _ in range(informations_reseau[i + 1])]))
-        B.append(np.array([[np.random.random_sample()] for _ in range(informations_reseau[i + 1])]))
+    def definition_reseau(self):
 
-    return V, A, W, B
+        print("\nDEFINITION DU RESEAU")
+        A, V, W, B = [], [], [], []
 
-def propagation(informations_reseau, V, A, W, B):
-    for i in range(len(informations_reseau)-1):
-        A[i + 1] = np.dot(W[i], V[i]) + B[i]
-        V[i + 1] = sigmoid_V(A[i+1])
-    return A, V
+        A.append(np.array([[None] for _ in range(self.informations_reseau[0])]))
+        V.append(np.array([[None] for _ in range(self.informations_reseau[0])]))
 
-def retropropagation(valeurs_theoriques, A, V, W, B):
+        for i in range(len(self.informations_reseau) - 1):
+            A.append(np.array([[None] for _ in range(self.informations_reseau[i + 1])]))
+            V.append(np.array([[None] for _ in range(self.informations_reseau[i + 1])]))
 
-    dV = [None] * len(V)
-    dW = [None] * len(W)
-    dB = [None] * len(B)
+            W.append(np.random.uniform(-1,1,(self.informations_reseau[i + 1], self.informations_reseau[i])))
+            B.append(np.random.uniform(-1,1,(self.informations_reseau[i + 1], 1)))
+        return A, V, W, B
 
-    dV[-1] = derivee_cout(V[-1], valeurs_theoriques) * derivee_sigmoid(A[-1])
-    dW[-1] = np.dot(dV[-1], V[-2].T)
-    dB[-1] = dV[-1]
+    def affichage_reinitialisation(self, total):
+        self.nombre_actuel = 0
+        self.nombre_total = total
+        self.nombre_affiche = -1
 
-    for k in range(2, len(V)):
-        dV[-k] = np.dot(W[-k + 1].T, dV[-k + 1]) * derivee_sigmoid(V[-k])
-        dW[-k] = np.dot(dV[-k], V[-k - 1].T)
-        dB[-k] = dV[-k]
+    def affichage(self):
+        calcul = int(self.nombre_actuel / self.nombre_total * 10)
+        if calcul > self.nombre_affiche:
+            self.nombre_affiche += 1
+            print("Progression : ", self.nombre_affiche * 10, "%")
+    def propagation(self, x):
+        if x.shape[1] != 1:
+            x = matrice_to_colonne(x)
 
-    return dW, dB
+        self.A[0] = x
+        self.V[0] = x
 
+        for i in range(len(self.informations_reseau) - 1):
+            self.A[i + 1] = np.dot(self.W[i], self.V[i]) + self.B[i]
+            self.V[i + 1] = self.fonction(self.A[i + 1])
 
-def maj(informations_reseau, taux, W, B, dW, dB):
-    for i in range(len(informations_reseau) - 1):
-        W[i] = W[i] - taux * dW[i]
-        B[i] = B[i] - taux * dB[i]
-    return W, B
+    def retropropagation(self, y):
+        dV = [None] * len(self.V)
+        dW = [None] * len(self.W)
+        dB = [None] * len(self.B)
 
+        dV[-1] = derivee_cout(self.V[-1], y) * self.fonction(self.A[-1])
+        dW[-1] = np.dot(dV[-1], self.V[-2].T)
+        dB[-1] = dV[-1]
 
-def tour(informations_reseau, taux, A, V, W, B, valeurs):
-    V[0] = valeurs[0]
-    A[0] = valeurs[0]
+        for k in range(2, len(self.V)):
+            dV[-k] = np.dot(self.W[-k + 1].T, dV[-k + 1]) * self.fonction(self.V[-k])
+            dW[-k] = np.dot(dV[-k], self.V[-k - 1].T)
+            dB[-k] = dV[-k]
 
-    valeurs_theoriques = valeurs[1]
+        for i in range(len(self.informations_reseau) - 1):
+            self.W[i] = self.W[i] - self.taux_apprentissage * dW[i]
+            self.B[i] = self.B[i] - self.taux_apprentissage * dB[i]
 
-    A,V = propagation(informations_reseau, V, A, W, B)
+    def entrainement(self):
 
-    dW, dB = retropropagation(valeurs_theoriques, A, V, W, B)
+        print("\nDEBUT DE L'ENTRAINEMENT")
 
-    W, B = maj(informations_reseau, taux, W, B, dW, dB)
+        self.affichage_reinitialisation(len(self.x_train))
 
-    return W, B
+        for k in range(len(self.x_train)):
+            self.nombre_actuel += 1
+            self.affichage()
+            self.propagation(self.x_train[k])
+            self.retropropagation(ligne_to_colonne(self.y_train[k]))
 
-def entrainement(informations_reseau, taux_apprentissage, donnees_entree):
-    valeurs_entree, valeurs_theoriques = donnees_entree
+        print("FIN DE L'ENTRAINEMENT")
 
-    jeu_taille = len(donnees_entree)
+    def test(self):
 
+        print("\nDEBUT DU TEST")
 
+        self.affichage_reinitialisation(len(self.x_test))
 
-    V, A, W, B = definition_reseau(valeurs_entree)
+        for k in range(self.taille_jeu_test):
+            self.propagation(self.x_test[k])
 
-    for k in range(jeu_taille):
-        W, B = tour(informations_reseau, taux_apprentissage, A, V, W, B, donnees_entree[k])
+            self.nombre_actuel += 1
+            self.affichage()
 
-    print("L'entrainement est terminé !")
+            valeur_pratique = np.argmax(self.V[-1])
+            valeur_theorique = np.argmax(self.y_test[k])
 
-    return A,V,W,B
+            if valeur_theorique == valeur_pratique:
+                self.nombre_succes += 1
 
-def resultat(M):
-    return np.argmax(M)
-
-def test(informations_reseau, donnees, V, A, W, B):
-
-    images, valeurs = donnees
-    succes, taille_jeu = 0, len(images)
-
-    for k in range(taille_jeu):
-        V[0] = images[k][0]
-        A[0] = images[k][0]
-
-        A,V = propagation(informations_reseau, V, A, W, B)
-
-        valeur_pratique = resultat(V[-1])
-        valeur_theorique = valeurs[k]
-
-        if valeur_theorique == valeur_pratique:
-            succes += 1
-
-    print("Le test est terminé !")
-
-    return round(succes/taille_jeu, 4)
-
-def reseau(informations_reseau, donnees_entrainement, donnees_test):
-    # Taille colonne 1, colonne 2, ...
-
-    taux_apprentissage = np.random.random_sample(4)
-
-    print("")
-    print("Entrainement")
-    print("")
-
-    A,V,W,B = entrainement(informations_reseau, taux_apprentissage, donnees_entrainement)
-
-    print("")
-    print("Test")
-    print("")
-    
-    resultat = reseau(informations_reseau, donnees_test, V, A, W, B)
-
-    return resultat
+        print("FIN DU TEST")
+        print("\nNombre de succès : ", self.nombre_succes)
+        print("Taux de réussite : ", self.nombre_succes / self.taille_jeu_test * 100, "%")
