81e07a674770e64c511cf710c44d78ffaa5d8157
Thomas <126348654+Thomas-kum@users.noreply.github.com>
2024-06-05 20:45:58 +0000

    Corrections diverses

--- Maillots1.py
+++ Maillots1.py
@@ -1,5 +1,6 @@
 from ReseauConvolution import ReseauConvolutif
 from MaillotsData import images, valeurs
+from annexe import ReLu, softmax, lineaire
 
 
 x_train, y_train = images[:1000], valeurs[:1000]
@@ -8,7 +9,12 @@ x_test, y_test = images[1000:], valeurs[1000:]
 x_train = x_train.astype("float32") / 255
 x_test = x_test.astype("float32") / 255
 
-taille = (32-2*5)
+reseau = ReseauConvolutif(45, [
+    ("C", ReLu),
+    ("C", lineaire),
+    ("D", ReLu),
+    ("D", softmax),
+    ("D", lineaire)
+], 0.01, 1000, (x_train, y_train))
 
-reseau = ReseauConvolutif(45, 32, 2, 0.01)
-reseau.entrainement((x_train, y_train), 10)
+reseau.entrainement()
